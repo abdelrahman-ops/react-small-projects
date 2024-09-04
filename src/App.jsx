e9d8eb4ce46b6fead134cb7afc286f42bf3a48ff
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-09-04 19:22:44 +0300

    the app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Show from './components/Show';
+import Timer from './components/Timer'
+import Todo from './components/Todo';
+
+function App() {
+    
+
+    return (
+        <div>
+            <Show />
+            <Timer />
+            <Todo />
+        </div>
+    )
+}
+
+export default App
